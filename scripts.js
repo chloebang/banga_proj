b0f5fdbaede920b6485089dd50b19c2312778aef
chloebang <126551420+chloebang@users.noreply.github.com>
2023-04-18 14:49:29 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function check(form)
+{
+ 
+ if(form.userid.value == null && form.pswrd.value == null)
+  {
+    alert("Please enter username and password.")
+  }
+ else
+ {
+   alert("Please enter username and password.")
+  }
+}
